193a9ab70fa157915a9a4ba89deab396bfa060c0
sinhnx.dev <contact@sinhnx.dev>
2025-10-07 15:44:22 +0700

    refactor: update console messages in PrintPdfToPrinter for clarity

--- working-with-pdf/PdfWithLabelPrinter/PrintPdfToPrinter.cs
+++ working-with-pdf/PdfWithLabelPrinter/PrintPdfToPrinter.cs
@@ -19,28 +19,28 @@ public class PrintPdfToPrinter
         switch (os.Platform)
         {
             case PlatformID.Win32S:
-                Console.WriteLine("Running on Win32s");
+                Console.WriteLine("Printing on Win32s");
                 break;
             case PlatformID.Win32Windows:
-                Console.WriteLine("Running on Windows 95/98/ME");
+                Console.WriteLine("Printing on Windows 95/98/ME");
                 break;
             case PlatformID.Win32NT:
-                Console.WriteLine("Running on Windows NT/2000/XP/Vista/7/8/10/11");
+                Console.WriteLine("Printing on Windows NT/2000/XP/Vista/7/8/10/11");
                 var printTimeout = new TimeSpan(0, 30, 0);
                 var printer = new PDFtoPrinterPrinter();
-                printer.Print(new PrintingOptions("Clabel- CT221B", filename), printTimeout);
+                printer.Print(new PrintingOptions("Clabel-CT221B", filename), printTimeout);
                 break;
             case PlatformID.WinCE:
-                Console.WriteLine("Running on Windows CE");
+                Console.WriteLine("Printing on Windows CE");
                 break;
             case PlatformID.MacOSX:
-                Console.WriteLine("Running on macOS");
+                Console.WriteLine("Printing on macOS");
                 // macos
                 System.Diagnostics.Process.Start("lpr", filename);
                 break;
             case PlatformID.Unix:
-                Console.WriteLine("Running on Unix/Linux");
-                System.Diagnostics.Process.Start("lpr", "customSizePdf.pdf");      
+                Console.WriteLine("Printing on Unix/Linux");
+                System.Diagnostics.Process.Start("lpr", filename);     
                 break;
             default:
                 Console.WriteLine("Unknown platform");
